cc7d2df250bad2921627bf052f563644bb44ca9e
Iannn <iannn@users.noreply.github.com>
2021-08-07 00:24:13 -0700

    All but T5 rare salvage included, and "other"

--- SalvageScripts/calc_salvage.py
+++ SalvageScripts/calc_salvage.py
@@ -38,7 +38,7 @@ Call function to output final report
 """API item and number
 
 Salvage items:
-    Metal
+    Metal Salvage
         21690=Brittle Clump of Ore
         21678=Bit of Metal Scrap
         21691=Weak Clump of Ore
@@ -58,7 +58,7 @@ Salvage items:
         79079=Unstable Metal Chunk
 
 
-    Leather
+    Leather Salvage
         21661=Tattered Hide
         21684=Rawhide Leather Strap
         21653=Tattered Pelt
@@ -83,16 +83,57 @@ Salvage items:
         21689=Hard Leather Strap
         21658=Salvageable Pelt
 
-    Cloth
-        aaaaa
+        79213=Unstable Hide
+        80681=Bloodstone-Warped Hide
 
 
-    Wood
+    Cloth Salvage
+        21669=Shredded Garment
+        22325=Half-Eaten Mass
+        21659=Shredded Rag
+
+        21671=Worn Garment
+        22326=Decaying Mass
+        21660=Worn Rag
+
+        21672=Ragged Garment
+        22327=Fetid Mass
+        21662=Soiled Rag
+
+        21673=Frayed Garment
+        22328=Malodorous Mass
+        21663=Frayed Rag
+
+        21674=Torn Garment
+        22329=Half-Digested Mass
+        21665=Torn Rag
+
+        79138=Unstable Rag
+
+
+    Wood Salvage
         79423=Reclaimed Wood Chunk
 
 
+    Rare Metal Salvage
+        43552=Bit of Aetherized Metal Scrap
+        41733=Bit of Fused Metal Scrap
+        45039=Bit of Twisted Watchwork Scrap
+
+        43553=Pile of Aetherized Metal Scrap
+        41734=Pile of Fused Metal Scrap
+        45040=Pile of Twisted Watchwork Scrap
+
+        43554=Jagged Aetherized Metal Scrap
+        41735=Jagged Fused Metal Scrap
+        45041=Jagged Twisted Watchwork Scrap
+
+        43555=Aetherized Metal Scrap
+        41736=Fused Metal Scrap
+        45042=Twisted Watchwork Scrap
+
 Direct salvage output material:
-    Metal
+    Metal Material
         19697=Copper Ore
         19703=Silver Ore
         19699=Iron Ore
@@ -101,7 +142,7 @@ Direct salvage output material:
         19700=Mithril Ore
         19701=Orichalcum Ore
 
-    Leather
+    Leather Material
         19719=Rawhide Leather Section
         19728=Thin Leather Section
         19730=Coarse Leather Section
@@ -109,15 +150,47 @@ Direct salvage output material:
         19729=Thick Leather Section
         19732=Hardened Leather Section
 
-    Cloth
-        aaaaaaaaaa
+    Cloth Material
+        19718=Jute Scrap
+        19739=Wool Scrap
+        19741=Cotton Scrap
+        19743=Linen Scrap
+        19748=Silk Scrap
+        19745=Gossamer Scrap
 
-    Wood
+    Wood Material
         19723=Green Wood Log
+        19726=Soft Wood Log
+        19727=Seasoned Wood Log
+        19724=Hard Wood Log
+        19722=Elder Wood Log
+        19725=Ancient Wood Log
+
+    Rare Salvage Materials
+        24301=Charged Sliver
+        24302=Charged Fragment
+        24303=Charged Shard
+        24304=Charged Core
+
+        24311=Molten Sliver
+        24312=Molten Fragment
+        24313=Molten Shard
+        24314=Molten Core
+
+        24316=Glacial Sliver
+        24317=Glacial Fragment
+        24318=Glacial Shard
+        24319=Glacial Core
+
+        24307=Onyx Fragment
+        24308=Onyx Shard
+        24309=Onyx Core
+
+        44941=Watchwork Sprocket
 
 
 Refinement materials:
-    Metal
+    Metal Refinement
         19680=Copper Ingot
         19679=Bronze Ingot
         19687=Silver Ingot
@@ -129,7 +202,7 @@ Refinement materials:
         19684=Mithril Ingot
         19685=Orichalcum Ingot
 
-    Leather
+    Leather Refinement
         19738=Stretched Rawhide Leather Square
         19733=Cured Thin Leather Square
         19734=Cured Coarse Leather Square
@@ -137,11 +210,21 @@ Refinement materials:
         19735=Cured Thick Leather Square
         19737=Cured Hardened Leather Square
 
-    Cloth
-        aaaaaaa
+    Cloth Refinement
+        19720=Bolt of Jute
+        19740=Bolt of Wool
+        19742=Bolt of Cotton
+        19744=Bolt of Linen
+        19747=Bolt of Silk
+        19746=Bolt of Gossamer
 
-    Wood
-
+    Wood Refinement
+        19710=Green Wood Plank
+        19713=Soft Wood Plank
+        19714=Seasoned Wood Plank
+        19711=Hard Wood Plank
+        19709=Elder Wood Plank
+        19712=Ancient Wood Plank
 
 Additional refinement materials:
     =Lump of Tin
@@ -173,10 +256,20 @@ Leather
     1x Cured Hardened Leather Square = 3x Hardened Leather Section
 
 Cloth
-    1x
+    1x Bolt of Jute = 2x Jute Scrap
+    1x Bolt of Wool = 2x Wool Scrap
+    1x Bolt of Cotton = 2x Cotton Scrap
+    1x Bolt of Linen = 2x Linen Scrap
+    1x Bolt of Silk = 3x Silk Scrap
+    1x Bolt of Gossamer = 2x Gossamer Scrap
 
 Wood
-    1x
+    1x Green Wood Plank = 3x Green Wood Log
+    1x Soft Wood Plank = 2x Soft Wood Log
+    1x Seasoned Wood Plank = 3x Seasoned Wood Plank
+    1x Hard Wood Plank = 3x Hard Wood Log
+    1x Elder Wood Plank = 3x Elder Wood Log
+    1x Ancient Wood Plank = 3x Ancient Wood Log
 
 """
 
@@ -224,41 +317,41 @@ Metal Salvage:
         Bloodstone-Warped Hide = Rawhide, Thin Leather, Coarse, Rugged, Thick, Hardened
 
     Cloth
-        Shredded Garment
-        Half-Eaten Mass
-        Shredded Rag
-        Worn Garment
-        Decaying Mass
-        Worn Rag
-        Ragged Garment
-        Fetid Mass
-        Soiled Rag
-        Frayed Garment
-        Malodorous Mass
-        Frayed Rag
-        Torn Garment
-        Half-Digested Mass
-        Torn Rag
-        Discarded Garment
-        Regurgitated Mass
-        Rag
-        Unstable Rag
+        Shredded Garment = (1-3) Jute Scrap
+        Half-Eaten Mass = (1-3) Jute Scrap
+        Shredded Rag = (1-3) Jute Scrap
+        Worn Garment = (1-3) Jute Scrap + (1-3) Wool Scrap
+        Decaying Mass = (1-3) Jute Scrap + (1-3) Wool Scrap
+        Worn Rag = (1-3) Jute Scrap + (1-3) Wool Scrap
+        Ragged Garment = (1-3) Wool Scrap + (1-3) Cotton Scrap
+        Fetid Mass = (1-3) Wool Scrap + (1-3) Cotton Scrap
+        Soiled Rag = (1-3) Wool Scrap + (1-3) Cotton Scrap
+        Frayed Garment = (1-3) Cotton Scrap + (1-3) Linen Scrap
+        Malodorous Mass = (1-3) Cotton Scrap + (1-3) Linen Scrap
+        Frayed Rag = (1-3) Cotton Scrap + (1-3) Linen Scrap
+        Torn Garment = (1-3) Linen Scrap + (1-3) Silk Scrap
+        Half-Digested Mass = (1-3) Linen Scrap + (1-3) Silk Scrap
+        Torn Rag = (1-3) Linen Scrap + (1-3) Silk Scrap
+        Discarded Garment = (1-3) Silk Scrap + Gossamer Scrap
+        Regurgitated Mass = (1-3) Silk Scrap + Gossamer Scrap
+        Rag = (1-3) Silk Scrap + Gossamer Scrap
+        Unstable Rag = Jute, Woll, Cotton, Linen, Silk, Gossamer
 
     Wood
         Reglaimed Wood Chunk = Green, Soft, Seasoned, Hard, Elder, Ancient
 
     Rare Metal Salvage
-        Bit or Aetherized Metal
-        Bit of Fused Metal Scrap
-        Bit of Twisted Watchwork Scrap
-        Pile of Aetherized Metal Scrap
-        Pile of Fused Metal Scrap
-        Pile of Twisted Watchwork Scrap
-        Jagged Aetherized Metal Scrap
-        Jagged Fused Metal Scrap
-        Jagged Twisted Watchwork Scrap
-        Aetherized Metal Scrap
-        Fused Metal Scrap
+        Bit or Aetherized Metal = Copper, Silver, Iron, Gold, Mithril, Charged Sliver
+        Bit of Fused Metal Scrap = Copper, Silver, Iron, Mithril, Molten Sliver, Glacial Sliver
+        Bit of Twisted Watchwork Scrap = Copper, Silver, Iron, Charged Sliver, Watchwork Sprocket
+        Pile of Aetherized Metal Scrap = Silver, Iron, Gold, Charged Fragment
+        Pile of Fused Metal Scrap = Silver, Iron, Gold, Molten Fragment, Glacial Fragment
+        Pile of Twisted Watchwork Scrap = Silver, Iron, Gold, Onyx Fragment, Watchwork Sprocket
+        Jagged Aetherized Metal Scrap = Iron, Gold, Platinum, Charged Shard
+        Jagged Fused Metal Scrap = Iron, Gold, Platinum, Molten Shard, Glaciak Shard
+        Jagged Twisted Watchwork Scrap = Iron, Gold, Platinum, Charged Shard, Onyx Shard, Watchwork Sprocket
+        Aetherized Metal Scrap = Silver, Iron, Gold, Platinum, Mithril, Orichalcum, Charged Sliver, Charged Fragment, Charged Core
+        Fused Metal Scrap = Copper, Iron, Platinum, Mithril, Orichalcum, Molten Sliver, Molten Core, Glacial Core
         Twisted Watchwork Scrap
         Salvageable Aetherized Metal Scrap
         Salvageable Fused Metal Scrap
@@ -281,29 +374,59 @@ def getMetal():
 
 
 def getLeather():
-    #api call for all metal TP prices
+    #api call for all leather TP prices
     #separate into base and refined
     #return format is a dict
     return
 
 
 def getCloth():
-    #api call for all metal TP prices
+    #api call for all cloth TP prices
     #separate into base and refined
     #return format is a dict
     return
 
 
 def getWood():
-    #api call for all metal TP prices
+    #api call for all wood TP prices
     #separate into base and refined
     #return format is a dict
     return
 
 
-def compareRefined():
-    #compare prices of raw vs refined
-    #separate buy and sell listings for reference because time is also money
+def compareMetal():
+    #compare prices of raw vs refined metal
+    #return dict with key metal and TP price values
+    #return another dict with key metal and something indicating processing
+    #metal-value will be used to calculate profits
+    #metal-processing is the all important instructions for me
+    return
+
+
+def compareLeather():
+    #compare prices of raw vs refined leather
+    #return dict with key leather and TP price values
+    #return another dict with key leather and something indicating processing
+    #leather-value will be used to calculate profits
+    #leather-processing is the all important instructions for me
+    return
+
+
+def compareCloth():
+    #compare prices of raw vs refined cloth
+    #return dict with key cloth and TP price values
+    #return another dict with key cloth and something indicating processing
+    #cloth-value will be used to calculate profits
+    #cloth-processing is the all important instructions for me
+    return
+
+
+def compareWood():
+    #compare prices of raw vs refined wood
+    #return dict with key wood and TP price values
+    #return another dict with key wood and something indicating processing
+    #wood-value will be used to calculate profits
+    #wood-processing is the all important instructions for me
     return
 
 
