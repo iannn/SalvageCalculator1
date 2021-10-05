e3694d6cde8ce01af072e5c797a748a1a606ba87
Iannn <iannn@users.noreply.github.com>
2021-10-04 22:09:08 -0700

    Moved common Unid functions to helper file

--- SalvageScripts/calc_unid.py
+++ SalvageScripts/calc_unid.py
@@ -206,54 +206,8 @@ def sort_allAPI(allAPI):
             print(entryAPI)
 
         #Decision all finished
-    return unid_prices,unrefined_prices,refined_prices,other_prices
+    return unid_prices,unrefined_prices,refined_prices
 #End of sort_allAPI
-
-def generate_multiplier(unrefined_dct,refined_dct,refined_scaler,refined_lookup,buysell):
-    """Input:
-        unrefined material prices dictionary
-        refined material prices dictionary
-        the scaler values to get value of raw material from refined
-        lookup dictionary to get refined from unrefined
-        buysell value. It is the array position 0=buy 1=sell"""
-
-    """Output:
-        multiplier for the best value/raw material
-        the decision to refine or not"""
-
-    #value per RAW MATERIAL goes into the multiplier dict, decision of raw/refined goes into decision dict
-    #Compare and finalize values of material
-    decision = {}
-    multiplier_prices = {}
-
-    for material_key in unrefined_dct:
-        if material_key in refined_scaler:
-            if unrefined_dct[material_key][buysell] >= refined_dct[refined_lookup[material_key]][buysell]/refined_scaler[material_key]:
-                decision[material_key] = 'raw'
-                multiplier_prices[material_key] = round(unrefined_dct[material_key][buysell],4)
-            else:
-                decision[material_key] = 'refined'
-                multiplier_prices[material_key] = round(refined_dct[refined_lookup[material_key]][buysell]/refined_scaler[material_key],4)
-        else:#This assumes that this was part of the "other materials" dict ie charm, symbol, ecto
-            decision[material_key]='none'
-            multiplier_prices[material_key]=unrefined_dct[material_key][buysell]
-
-    return multiplier_prices,decision
-#end of generate_multiplier
-
-def compute_result(droprate_dict,multiplier_dict,TPCut):
-    salvageValue_dct = {}
-    sum_val = 0
-    if TPCut == True:
-        TPValue = 0.85
-    else:
-        TPValue = 1
-
-    for key in droprate_dict:
-        salvageValue_dct[key] = round(TPValue*droprate_dict[key]*multiplier_dict[key],4)
-        sum_val = sum_val + salvageValue_dct[key]
-
-    return salvageValue_dct,sum_val
 
 def unidPrint(droprate_dict,salvageCost_val,unid_name,unid_price,sum_val):
     print('{print_outname:<16}: Buy order = {print_cost}; Average salvage cost = {print_salvageCost}; Average salvage value = {print_salvageValue}; Estimated {print_profit} profit per salvage'.format(print_outname=unid_name,print_cost=unid_price, print_salvageValue=round(sum_val,4), print_profit=round(sum_val-unid_price-salvageCost_val,4), print_salvageCost=salvageCost_val))
@@ -263,6 +217,9 @@ def unidPrint(droprate_dict,salvageCost_val,unid_name,unid_price,sum_val):
 Main Program start
 """
 
+#Common helper file
+from calc_helpers import *
+#Python GW2 API wrapper library
 from gw2api import GuildWars2Client
 gw2_client = GuildWars2Client()
 
@@ -288,9 +245,6 @@ unidRare_salvageCost = salvageCost['Silver']*1
 unrefined_to_refined = {'Orichalcum Ore':'Orichalcum Ingot','Ancient Wood Log':'Ancient Wood Plank','Gossamer Scrap':'Bolt of Gossamer','Hardened Leather Section':'Cured Hardened Leather Square','Mithril Ore':'Mithril Ingot','Elder Wood Log':'Elder Wood Plank','Silk Scrap':'Bolt of Silk','Thick Leather Square':'Cured Thick Leather Square','Lucent Mote':'Pile of Lucent Crystal'}
 #Everything is based off of the raw material so use raw material as lookup.
 refined_scaler = {'Orichalcum Ore':2,'Ancient Wood Log':3,'Gossamer Scrap':2,'Hardened Leather Section':3,'Mithril Ore':2,'Elder Wood Log':3,'Silk Scrap':3,'Thick Leather Square':4,'Lucent Mote':10}
-
-
-
 
 
 allIDs = [85016,84731,83008,#unidentified gear
