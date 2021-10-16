ece918a5d0f18e8a363d1782896ec7660f9a2669
Iannn <iannn@users.noreply.github.com>
2021-10-15 23:48:35 -0700

    Carefully typed out copper multiplier case. Iron started. Need to test

--- SalvageScripts/calc_helpers.py
+++ SalvageScripts/calc_helpers.py
@@ -13,7 +13,9 @@ def generate_multiplier(unrefined_dct,refined_dct,refined_scalar,refined_lookup,
         multiplier for the best value/raw material
         the decision to refine or not"""
 
-    """Application note: raw values only can be forced if no "refined_scalar " """""
+    """Application note:
+            raw values only can be forced if "refined_scalar" is an empty dict
+            dictionaries passed are passed by reference so altering refined_lookup with the Tin/Coal/Primordium variants can be done inside here fine"""""
 
     #value per RAW MATERIAL goes into the multiplier dict, decision of raw/refined goes into decision dict
     #Compare and finalize values of material
@@ -24,9 +26,32 @@ def generate_multiplier(unrefined_dct,refined_dct,refined_scalar,refined_lookup,
         if material_key in refined_scalar:
             if material_key == 'Copper Ore':
                 #copper ore is largest price
-                if (unrefined_dct['Copper Ore'][buysell] >= refined_dct[]) && ():
-                    decision_dct[material_key] = 'raw'
-                    multiplier_dct[material_key] = round(unrefined_dct[material_key][buysell],4)
+                #5 bronze per lump of tin. Tin bought 10 for 80c
+                if (unrefined_dct['Copper Ore'][buysell] >= refined_dct['Copper Ingot'][buysell])/refined_scalar['Copper Ingot'] && (unrefined_dct['Copper Ore'][buysell] >= (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot']):
+                    decision_dct['Copper Ore'] = 'raw'
+                    multiplier_dct['Copper Ore'] = round(unrefined_dct['Copper Ore'][buysell],4)
+                #bronze > copper
+                elif (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot'] > refined_dct[refined_lookup[material_key]][buysell]/refined_scalar[refined_lookup[material_key]]:
+                    decision_dct['Copper Ore'] = 'B Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Bronze Ingot'][buysell],4)
+                    unrefined_to_refined['Copper Ore']='Bronze Ingot'#Passed by reference so update persists
+                else:
+                    decision_dct['Copper Ore'] = 'C Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Copper Ingot'][buysell],4)
+            elif material_key == 'Iron Ore':
+                #copper ore is largest price
+                #5 bronze per lump of tin. Tin bought 10 for 80c
+                if (unrefined_dct['Iron Ore'][buysell] >= refined_dct['Copper Ingot'][buysell])/refined_scalar['Copper Ingot'] && (unrefined_dct['Copper Ore'][buysell] >= (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot']):
+                    decision_dct['Iron Ore'] = 'raw'
+                    multiplier_dct['Iron Ore'] = round(unrefined_dct['Copper Ore'][buysell],4)
+                #bronze > copper
+                elif (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot'] > refined_dct[refined_lookup[material_key]][buysell]/refined_scalar[refined_lookup[material_key]]:
+                    decision_dct['Copper Ore'] = 'B Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Bronze Ingot'][buysell],4)
+                    unrefined_to_refined['Copper Ore']='Bronze Ingot'#Passed by reference so update persists
+                else:
+                    decision_dct['Copper Ore'] = 'C Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Copper Ingot'][buysell],4)
             elif unrefined_dct[material_key][buysell] >= refined_dct[refined_lookup[material_key]][buysell]/refined_scalar[refined_lookup[material_key]]:
                 decision_dct[material_key] = 'raw'
                 multiplier_dct[material_key] = round(unrefined_dct[material_key][buysell],4)
