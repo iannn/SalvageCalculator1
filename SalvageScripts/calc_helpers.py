af987c3e36b11d14f259ba43116b6cd3c65582bf
Iannn <iannn@users.noreply.github.com>
2021-10-19 22:34:55 -0700

    This wasn't my version from refactor-results branch. Copied from github

--- SalvageScripts/calc_helpers.py
+++ SalvageScripts/calc_helpers.py
@@ -1,11 +1,11 @@
 #Helper function file for GW2 salvaging for profit scripts
 #Common functions only
 
-def generate_multiplier(unrefined_dct,refined_dct,refined_scaler,refined_lookup,buysell):
+def generate_multiplier(unrefined_dct,refined_dct,refined_scalar,refined_lookup,buysell):
     """Input:
         unrefined material prices dictionary
         refined material prices dictionary
-        the scaler values to get value of raw material from refined
+        the scalar values to get value of raw material from refined
         lookup dictionary to get refined from unrefined
         buysell value. It is the array position 0=buy 1=sell"""
 
@@ -13,24 +13,73 @@ def generate_multiplier(unrefined_dct,refined_dct,refined_scaler,refined_lookup,
         multiplier for the best value/raw material
         the decision to refine or not"""
 
+    """Application note:
+            raw values only can be forced if "refined_scalar" is an empty dict
+            dictionaries passed are passed by reference so altering refined_lookup with the Tin/Coal/Primordium variants can be done inside here fine"""""
+
     #value per RAW MATERIAL goes into the multiplier dict, decision of raw/refined goes into decision dict
     #Compare and finalize values of material
-    decision = {}
-    multiplier_prices = {}
+    decision_dct = {}
+    multiplier_dct = {}
 
     for material_key in unrefined_dct:
-        if material_key in refined_scaler:
-            if unrefined_dct[material_key][buysell] >= refined_dct[refined_lookup[material_key]][buysell]/refined_scaler[material_key]:
-                decision[material_key] = 'raw'
-                multiplier_prices[material_key] = round(unrefined_dct[material_key][buysell],4)
+        if material_key in refined_lookup:
+            if material_key == 'Copper Ore':
+                #copper ore is largest price
+                #5 bronze per lump of tin. Tin bought 10 for 80c
+                if (unrefined_dct['Copper Ore'][buysell] >= refined_dct['Copper Ingot'][buysell])/refined_scalar['Copper Ingot'] and (unrefined_dct['Copper Ore'][buysell] >= (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot']):
+                    decision_dct['Copper Ore'] = 'raw'
+                    multiplier_dct['Copper Ore'] = round(unrefined_dct['Copper Ore'][buysell],4)
+                #Bronze > Copper Ingot
+                elif (refined_dct['Bronze Ingot'][buysell]-8/5)/refined_scalar['Bronze Ingot'] > refined_dct['Copper Ingot'][buysell]/refined_scalar['Copper Ingot']:
+                    decision_dct['Copper Ore'] = 'B Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Bronze Ingot'][buysell],4)
+                    unrefined_to_refined['Copper Ore']='Bronze Ingot'#Passed by reference so update persists
+                #Regular copper ingot is therefore the best option
+                else:
+                    decision_dct['Copper Ore'] = 'C Ingot'
+                    multiplier_dct['Copper Ore'] = round(refined_dct['Copper Ingot'][buysell],4)
+            elif material_key == 'Iron Ore':
+                #Iron ore is largest price
+                #1 lump of coal per Steel Ingot. Coal bought 10 for 160c
+                if (unrefined_dct['Iron Ore'][buysell] >= refined_dct['Iron Ingot'][buysell])/refined_scalar['Iron Ingot'] and (unrefined_dct['Iron Ore'][buysell] >= (refined_dct['Steel Ingot'][buysell]-16)/refined_scalar['Steel Ingot']):
+                    decision_dct['Iron Ore'] = 'raw'
+                    multiplier_dct['Iron Ore'] = round(unrefined_dct['Iron Ore'][buysell],4)
+                #Steel > Iron Ingot
+                elif (refined_dct['Steel Ingot'][buysell]-16)/refined_scalar['Steel Ingot'] > refined_dct['Iron Ingot'][buysell]/refined_scalar['Iron Ingot']:
+                    decision_dct['Iron Ore'] = 'S Ingot'
+                    multiplier_dct['Iron Ore'] = round(refined_dct['Steel Ingot'][buysell],4)
+                    unrefined_to_refined['Iron Ore']='Bronze Ingot'#Passed by reference so update persists
+                #Regular Iron Ingot is therefore the best option
+                else:
+                    decision_dct['Iron Ore'] = 'I Ingot'
+                    multiplier_dct['Iron Ore'] = round(refined_dct['Iron Ingot'][buysell],4)
+            elif material_key == 'Platinum Ore':
+                #Platinum ore is largest price
+                #1 lump of Primordium per Darksteel Ingot. Primordium bought 10 for 480c
+                if (unrefined_dct['Platinum Ore'][buysell] >= refined_dct['Platinum Ingot'][buysell])/refined_scalar['Platinum Ingot'] and (unrefined_dct['Platinum Ore'][buysell] >= (refined_dct['Darksteel Ingot'][buysell]-48)/refined_scalar['Darksteel Ingot']):
+                    decision_dct['Platinum Ore'] = 'raw'
+                    multiplier_dct['Platinum Ore'] = round(unrefined_dct['Platinum Ore'][buysell],4)
+                #Darksteel > Platinum Ingot
+                elif (refined_dct['Darksteel Ingot'][buysell]-48)/refined_scalar['Darksteel Ingot'] > refined_dct['Platinum Ingot'][buysell]/refined_scalar['Platinum Ingot']:
+                    decision_dct['Platinum Ore'] = 'D Ingot'
+                    multiplier_dct['Platinum Ore'] = round(refined_dct['Darksteel Ingot'][buysell],4)
+                    unrefined_to_refined['Platinum Ore']='Darksteel Ingot'#Passed by reference so update persists
+                #Regular Platinum Ingot is therefore the best option
+                else:
+                    decision_dct['Platinum Ore'] = 'P Ingot'
+                    multiplier_dct['Platinum Ore'] = round(refined_dct['Platinum Ingot'][buysell],4)
+            elif unrefined_dct[material_key][buysell] >= refined_dct[refined_lookup[material_key]][buysell]/refined_scalar[refined_lookup[material_key]]:
+                decision_dct[material_key] = 'raw'
+                multiplier_dct[material_key] = round(unrefined_dct[material_key][buysell],4)
             else:
-                decision[material_key] = 'refined'
-                multiplier_prices[material_key] = round(refined_dct[refined_lookup[material_key]][buysell]/refined_scaler[material_key],4)
+                decision_dct[material_key] = 'refined'
+                multiplier_dct[material_key] = round(refined_dct[refined_lookup[material_key]][buysell]/refined_scalar[refined_lookup[material_key]],4)
         else:#This assumes that this was part of the "other materials" dict ie charm, symbol, ecto
-            decision[material_key]='none'
-            multiplier_prices[material_key]=unrefined_dct[material_key][buysell]
+            decision_dct[material_key]='none'
+            multiplier_dct[material_key]=unrefined_dct[material_key][buysell]
 
-    return multiplier_prices,decision
+    return multiplier_dct,decision_dct
 #end of generate_multiplier
 
 
@@ -47,3 +96,4 @@ def compute_result(droprate_dict,multiplier_dict,TPCut):
         sum_val = sum_val + salvageValue_dct[key]
 
     return salvageValue_dct,sum_val
+#End of compute_result
