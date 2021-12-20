2089eae5086f85e99e2468f36648feed7eb3c753
Iannn <iannn@users.noreply.github.com>
2021-12-19 20:37:59 -0800

    Reorganized unid while I was at it. Priority still salvage items

--- SalvageScripts/calc_unid.py
+++ SalvageScripts/calc_unid.py
@@ -107,6 +107,10 @@ https://wiki.guildwars2.com/wiki/Silver-Fed_Salvage-o-Matic
 
 """
 
+"""
+Function Declarations
+"""
+
 #Main gw2 API call return list of dics that may not always be in the same order so filter and arrange data for unids here
 def sort_allAPI(allAPI):
 
@@ -140,7 +144,7 @@ def sort_allAPI(allAPI):
         else:
             print("Unexpected API return")
             print(entryAPI)
-    
+
     return unid_prices,unrefined_prices,refined_prices
 #End of sort_allAPI
 
@@ -150,14 +154,8 @@ def unidPrint(droprate_dict,salvageCost_val,unid_name,unid_price,sum_val):
 #End of unidPrint
 
 """
-Main Program start
+Drop Rates
 """
-
-#Common helper file
-from calc_helpers import *
-#Python GW2 API wrapper library
-from gw2api import GuildWars2Client
-gw2_client = GuildWars2Client()
 
 #charm and symbol values I have are low data so using wiki values
 unidFine_droprate = {'Orichalcum Ore':0.0399,'Ancient Wood Log':0.0339,'Gossamer Scrap':0.0155,'Hardened Leather Section':0.0152,'Mithril Ore':0.4281,'Elder Wood Log':0.3869,'Silk Scrap':0.3013,'Thick Leather Square':0.2473,'Ectoplasm':0.0076,'Lucent Mote':0.1058,'Symbol of Control':0.0003,'Symbol of Enhancement':0.0002,'Symbol of Pain':0.0004,'Charm of Brilliance':0.0008,'Charm of Potence':0.0003,'Charm of Skill':0.0003}
@@ -176,12 +174,10 @@ unidFine_salvageCost = round(salvageCost['Mystic']*0.0100 + salvageCost['Runecra
 unidMasterwork_salvageCost = round(salvageCost['Mystic']*0.0336 + salvageCost['Runecrafter']*0.9638,4)
 unidRare_salvageCost = round(salvageCost['Silver']*0.9840,4)
 
-#Helper dictionaries
-#lookup unrefined to get refined
-unrefined_to_refined = {'Orichalcum Ore':'Orichalcum Ingot','Ancient Wood Log':'Ancient Wood Plank','Gossamer Scrap':'Bolt of Gossamer','Hardened Leather Section':'Cured Hardened Leather Square','Mithril Ore':'Mithril Ingot','Elder Wood Log':'Elder Wood Plank','Silk Scrap':'Bolt of Silk','Thick Leather Square':'Cured Thick Leather Square','Lucent Mote':'Pile of Lucent Crystal'}
-#Everything is based off of the raw material so use raw material as lookup.
-refined_scalar = {'Orichalcum Ingot':2,'Ancient Wood Plank':3,'Bolt of Gossamer':2,'Cured Hardened Leather Square':3,'Mithril Ingot':2,'Elder Wood Plank':3,'Bolt of Silk':3,'Cured Thick Leather Square':4,'Pile of Lucent Crystal':10}
 
+"""
+Helper Stuff
+"""
 
 allIDs = [85016,84731,83008,#unidentified gear
         19701,19725,19745,19732,#T6
@@ -193,38 +189,51 @@ allIDs = [85016,84731,83008,#unidentified gear
         19684,19709,19747,19735,#T5 refined
         89271]#Lucent crystal
 
+#lookup unrefined to get refined
+unrefined_to_refined = {'Orichalcum Ore':'Orichalcum Ingot','Ancient Wood Log':'Ancient Wood Plank','Gossamer Scrap':'Bolt of Gossamer','Hardened Leather Section':'Cured Hardened Leather Square','Mithril Ore':'Mithril Ingot','Elder Wood Log':'Elder Wood Plank','Silk Scrap':'Bolt of Silk','Thick Leather Square':'Cured Thick Leather Square','Lucent Mote':'Pile of Lucent Crystal'}
+#Everything is based off of the raw material so use raw material as lookup.
+refined_scalar = {'Orichalcum Ingot':2,'Ancient Wood Plank':3,'Bolt of Gossamer':2,'Cured Hardened Leather Square':3,'Mithril Ingot':2,'Elder Wood Plank':3,'Bolt of Silk':3,'Cured Thick Leather Square':4,'Pile of Lucent Crystal':10}
 
-allAPI=gw2_client.commerceprices.get(ids=allIDs)
+"""
+Main Program start
+"""
+if __name__ == '__main__':
+    #Common helper file
+    from calc_helpers import *
+    #Python GW2 API wrapper library
+    from gw2api import GuildWars2Client
+    gw2_client = GuildWars2Client()
 
-unid_prices,unrefined_prices,refined_prices = sort_allAPI(allAPI)
+    allAPI=gw2_client.commerceprices.get(ids=allIDs)
 
-#sell value prices and decisions. No TP cut yet
-multiplier_prices,decision = generate_multiplier(unrefined_prices,refined_prices,refined_scalar,unrefined_to_refined,1)
+    unid_prices,unrefined_prices,refined_prices = sort_allAPI(allAPI)
 
-#Post
-#TP cut included here
-unidFine_SalvageValue, unidFine_sum = compute_result(unidFine_droprate,multiplier_prices,True)
-unidMasterwork_salvageValue, unidMasterwork_sum = compute_result(unidMasterwork_droprate,multiplier_prices,True)
-unidRare_salvageValue, unidRare_sum = compute_result(unidRare_droprate,multiplier_prices,True)
+    #sell value prices and decisions. No TP cut yet
+    multiplier_prices,decision = generate_multiplier(unrefined_prices,refined_prices,refined_scalar,unrefined_to_refined,1)
 
-#make new table. Include decision description if decision present
-print('{:<24} : {:>10}   {:<10}   {:<5}   {:>10}   {:>10}   {:>10}   {:>10}'.format('Material','Sell Price','State','Raw','Refined','Fine','Masterwork','Rare'))
-print('-'*113)
-for key, value in multiplier_prices.items():
-    if key in unrefined_to_refined:
-        print('{:<24} : {:>10}   {:<10}   {:>5}   {:>10}   {:>10}   {:>10}   {:>10}'.format(key,value, decision[key],unrefined_prices[key][1],refined_prices[unrefined_to_refined[key]][1],unidFine_SalvageValue[key],unidMasterwork_salvageValue[key],unidRare_salvageValue[key]))
-    else:
-        print('{:<24} : {:>10}   {:<10}   {:>5}   {:>10}   {:>10}   {:>10}   {:>10}'.format(key,value, decision[key],'-','-',unidFine_SalvageValue[key],unidMasterwork_salvageValue[key],unidRare_salvageValue[key]))
+    #TP cut included here
+    unidFine_SalvageValue, unidFine_sum = compute_result(unidFine_droprate,multiplier_prices,True)
+    unidMasterwork_salvageValue, unidMasterwork_sum = compute_result(unidMasterwork_droprate,multiplier_prices,True)
+    unidRare_salvageValue, unidRare_sum = compute_result(unidRare_droprate,multiplier_prices,True)
 
-print('\nResult function: Sell')
+    #Table. All columns fixed because same material/options for each unid. Except luck. Luck doesn't drop from rares apparently
+    print('{:<24} : {:>10}   {:<10}   {:<5}   {:>10}   {:>10}   {:>10}   {:>10}'.format('Material','Sell Price','State','Raw','Refined','Fine','Masterwork','Rare'))
+    print('-'*113)
+    for key, value in multiplier_prices.items():
+        if key in unrefined_to_refined:
+            print('{:<24} : {:>10}   {:<10}   {:>5}   {:>10}   {:>10}   {:>10}   {:>10}'.format(key,value, decision[key],unrefined_prices[key][1],refined_prices[unrefined_to_refined[key]][1],unidFine_SalvageValue[key],unidMasterwork_salvageValue[key],unidRare_salvageValue[key]))
+        else:
+            print('{:<24} : {:>10}   {:<10}   {:>5}   {:>10}   {:>10}   {:>10}   {:>10}'.format(key,value, decision[key],'-','-',unidFine_SalvageValue[key],unidMasterwork_salvageValue[key],unidRare_salvageValue[key]))
+
+    print('\nResult function: Sell')
 
 
-unidPrint(unidFine_droprate,unidFine_salvageCost,'Fine',unid_prices['Fine'][0],unidFine_sum)
-unidPrint(unidMasterwork_droprate,unidMasterwork_salvageCost,'Masterwork',unid_prices['Masterwork'][0],unidMasterwork_sum)
-unidPrint(unidRare_droprate,unidRare_salvageCost,'Rare',unid_prices['Rare'][0],unidRare_sum)
+    unidPrint(unidFine_droprate,unidFine_salvageCost,'Fine',unid_prices['Fine'][0],unidFine_sum)
+    unidPrint(unidMasterwork_droprate,unidMasterwork_salvageCost,'Masterwork',unid_prices['Masterwork'][0],unidMasterwork_sum)
+    unidPrint(unidRare_droprate,unidRare_salvageCost,'Rare',unid_prices['Rare'][0],unidRare_sum)
 
-#print('\nBuy testing')
-#buymultiplier,buydecisions = generate_multiplier(unrefined_prices,refined_prices,refined_scalar,unrefined_to_refined,1)
-#compute_result(unidFine_droprate,multiplier_prices,unidFine_salvageCost,'Fine',unid_prices['Fine'][0])
+    #print('\nBuy testing')
+    #buymultiplier,buydecisions = generate_multiplier(unrefined_prices,refined_prices,refined_scalar,unrefined_to_refined,1)
+    #compute_result(unidFine_droprate,multiplier_prices,unidFine_salvageCost,'Fine',unid_prices['Fine'][0])
 
-print("The end")
+    print("The end")
