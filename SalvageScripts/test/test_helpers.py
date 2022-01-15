a969c3e9d2ff3214735334a9629056c2dbb78caf
Iannn <iannn@users.noreply.github.com>
2022-01-15 14:19:52 -0800

    generate_multiplier copper test case seems to work fine. Math also checked during creation/formatting

--- SalvageScripts/test/test_helpers.py
+++ SalvageScripts/test/test_helpers.py
@@ -46,17 +46,39 @@ generate_multiplier needs
     refined_scalar
     refined_lookup
 
-each individual case needs unique value combinations for:
+each individual case needs unique value combinations for (in this order):
     unrefined_dct = dictionary with unrefined material and price in the format of "name":[buy order price, sell listing price]
     refined_dct = dictionary with refined material and price in the format of "name":[buy order price, sell listing price]
     buysell = 0 or 1 representing the buy or sell price position
     expected_multiplier_dct = dictionary with expected value of material price with best value in the format of "name": price
     expected_decision_dct = dictionary with expected form of material in the format of "name":"format name"
+    additional refined_lookup for change/no change
+
+#Temporary test metal dictionary
+unrefined {'Copper Ore': [17, 23], 'Iron Ore': [72, 89], 'Platinum Ore': [91, 92]}
+refined {'Copper Ingot': [24, 25], 'Bronze Ingot': [38, 47], 'Silver Ingot': [12, 14], 'Iron Ingot': [149, 173], 'Steel Ingot': [227, 275], 'Gold Ingot': [27, 29], 'Platinum Ingot': [151, 180], 'Darksteel Ingot': [228, 280]}
 
 #Special Ore cases
-CopperOreWins = ()
-CopperIngotWins = ()
-BronzeIngotWins = ()
+CopperOreWins = ({'Copper Ore': [0, 0]},
+                {'Copper Ingot': [0, 0],'Bronze Ingot': [0, 0]},
+                1,
+                {'Copper Ore': 0},
+                {'Copper Ore': 'raw'},
+                {no change})
+
+CopperIngotWins = ({'Copper Ore': [0, 0]},
+                {'Copper Ingot': [0, 0],'Bronze Ingot': [0, 0]},
+                1,
+                {'Copper Ore': 0},
+                {'Copper Ore': 'C Ingot'},
+                {no change})
+
+BronzeIngotWins = ({'Copper Ore': [0, 0]},
+                {'Copper Ingot': [0, 0],'Bronze Ingot': [0, 0]},
+                1,
+                {'Copper Ore': 0},
+                {'Copper Ore': 'B Ingot'},
+                {change to Bronze Ingot})
 
 IronOreWins
 IronIngotWins
@@ -66,19 +88,57 @@ PlatinumOreWins
 PlatinumIngotWins
 DarksteelIngotWins
 
+#ancient wood test dicts
+'Ancient Wood Log': [161, 181]
+'Ancient Wood Plank': [506, 575]
+
 #regular material test
-AncientWoodLogWins
+AncientWoodLogWins = ()
 AncientWoodPlankWins
+
 
 #no refinement test
 SymbolOfControlWins
 
 """
-"""
-@pytest.mark.parametrize("test_unrefined_dct,test_refined_dct,test_buysell,expected_multiplier_dct,expected_decision_dct,expected_refined_lookup")
-def test_generatingMultiplier():
-    generate_multiplier(unrefined_dct,refined_dct,refined_scalar,refined_lookup,buysell)
-"""
+
+#Special Ore cases
+CopperOreWins = ({'Copper Ore': [0, 5]},
+                {'Copper Ingot': [0, 10],'Bronze Ingot': [0, 11.6]},
+                1,
+                {'Copper Ore': 5},
+                {'Copper Ore': 'raw'},
+                {'Hardened Leather Section':'Cured Hardened Leather Square','Thick Leather Section':'Cured Thick Leather Square','Rugged Leather Section':'Cured Rugged Leather Square','Coarse Leather Section':'Cured Coarse Leather Square','Thin Leather Section':'Cured Thin Leather Square','Rawhide Leather Section':'Stretched Rawhide Leather Square',
+                                        'Copper Ore':'Copper Ingot','Silver Ore':'Silver Ingot','Iron Ore':'Iron Ingot','Gold Ore':'Gold Ingot','Platinum Ore':'Platinum Ingot','Mithril Ore':'Mithril Ingot','Orichalcum Ore':'Orichalcum Ingot',
+                                        'Jute Scrap':'Bolt of Jute','Wool Scrap':'Bolt of Wool','Cotton Scrap':'Bolt of Cotton','Linen Scrap':'Bolt of Linen','Silk Scrap':'Bolt of Silk','Gossamer Scrap':'Bolt of Gossamer',
+                                        'Green Wood Log':'Green Wood Plank','Soft Wood Log':'Soft Wood Plank','Seasoned Wood Log':'Seasoned Wood Plank','Hard Wood Log':'Hard Wood Plank','Elder Wood Log':'Elder Wood Plank','Ancient Wood Log':'Ancient Wood Plank'})
+CopperIngotWins = ({'Copper Ore': [0, 1]},
+                {'Copper Ingot': [0, 12],'Bronze Ingot': [0, 2]},
+                1,
+                {'Copper Ore': 6},
+                {'Copper Ore': 'C Ingot'},
+                {'Hardened Leather Section':'Cured Hardened Leather Square','Thick Leather Section':'Cured Thick Leather Square','Rugged Leather Section':'Cured Rugged Leather Square','Coarse Leather Section':'Cured Coarse Leather Square','Thin Leather Section':'Cured Thin Leather Square','Rawhide Leather Section':'Stretched Rawhide Leather Square',
+                                        'Copper Ore':'Copper Ingot','Silver Ore':'Silver Ingot','Iron Ore':'Iron Ingot','Gold Ore':'Gold Ingot','Platinum Ore':'Platinum Ingot','Mithril Ore':'Mithril Ingot','Orichalcum Ore':'Orichalcum Ingot',
+                                        'Jute Scrap':'Bolt of Jute','Wool Scrap':'Bolt of Wool','Cotton Scrap':'Bolt of Cotton','Linen Scrap':'Bolt of Linen','Silk Scrap':'Bolt of Silk','Gossamer Scrap':'Bolt of Gossamer',
+                                        'Green Wood Log':'Green Wood Plank','Soft Wood Log':'Soft Wood Plank','Seasoned Wood Log':'Seasoned Wood Plank','Hard Wood Log':'Hard Wood Plank','Elder Wood Log':'Elder Wood Plank','Ancient Wood Log':'Ancient Wood Plank'})
+BronzeIngotWins = ({'Copper Ore': [0, 2]},
+                {'Copper Ingot': [0, 3],'Bronze Ingot': [0, 17.6]},
+                1,
+                {'Copper Ore': 8},
+                {'Copper Ore': 'B Ingot'},
+                {'Hardened Leather Section':'Cured Hardened Leather Square','Thick Leather Section':'Cured Thick Leather Square','Rugged Leather Section':'Cured Rugged Leather Square','Coarse Leather Section':'Cured Coarse Leather Square','Thin Leather Section':'Cured Thin Leather Square','Rawhide Leather Section':'Stretched Rawhide Leather Square',
+                                        'Copper Ore':'Bronze Ingot','Silver Ore':'Silver Ingot','Iron Ore':'Iron Ingot','Gold Ore':'Gold Ingot','Platinum Ore':'Platinum Ingot','Mithril Ore':'Mithril Ingot','Orichalcum Ore':'Orichalcum Ingot',
+                                        'Jute Scrap':'Bolt of Jute','Wool Scrap':'Bolt of Wool','Cotton Scrap':'Bolt of Cotton','Linen Scrap':'Bolt of Linen','Silk Scrap':'Bolt of Silk','Gossamer Scrap':'Bolt of Gossamer',
+                                        'Green Wood Log':'Green Wood Plank','Soft Wood Log':'Soft Wood Plank','Seasoned Wood Log':'Seasoned Wood Plank','Hard Wood Log':'Hard Wood Plank','Elder Wood Log':'Elder Wood Plank','Ancient Wood Log':'Ancient Wood Plank'})
+
+
+
+@pytest.mark.parametrize("test_unrefined_dct,test_refined_dct,test_buysell,expected_multiplier_dct,expected_decision_dct,expected_refined_lookup",[CopperOreWins])
+def test_generatingMultiplier(test_unrefined_dct,test_refined_dct,test_buysell,expected_multiplier_dct,expected_decision_dct,expected_refined_lookup,refined_scalar,refined_lookup):
+    multiplier_dct, decision_dct = calc_helpers.generate_multiplier(test_unrefined_dct,test_refined_dct,refined_scalar,refined_lookup,test_buysell)
+    assert multiplier_dct == expected_multiplier_dct
+    assert decision_dct == expected_decision_dct
+    assert refined_lookup == expected_refined_lookup
 
 """
 The purpose of testing is to check that the math of dictionary multiplication and summation is working correctly
